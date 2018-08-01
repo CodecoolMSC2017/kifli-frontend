163b82f5d517f2805cae686f08fff71bee3603ac
HajnalFeri <kalacsf1@gmail.com>
2018-08-01 11:02:00 +0200

    add click to home button and the search is works and lising on the home

--- project-kifli/src/app/search.service.ts
+++ project-kifli/src/app/search.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable }    from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
+  private searchTitleInput = new Subject<string>();
+  private getAllProduct = new Subject<boolean>();
+
   constructor() { }
+
+  searchTitle$ = this.searchTitleInput.asObservable();
+  getProductAds$ = this.getAllProduct.asObservable();
+
+  searchTitleApply(search: string) {
+    this.searchTitleInput.next(search);
+    console.log(search);
+  }
+
+  getAllProductClick(activate: boolean) {
+    this.getAllProduct.next(activate);
+  }
 }
