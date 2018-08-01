2033088db0e0213648f4e50d823baabcd11bd2c8
dnorbert4000 <dnorbert@gmail.com>
2018-08-01 11:56:00 +0200

    the products of the user listing under cunstruction

--- project-kifli/src/app/profile/profile.component.ts
+++ project-kifli/src/app/profile/profile.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Credentials} from '../credentials';
+import { BrowserModule } from '@angular/platform-browser'; 
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
+@NgModule({
+    imports: [     
+      BrowserModule,
+      HttpClientModule,
+    ]
+  }) 
+
 export class ProfileComponent implements OnInit {
   profile;
+  userId: String;
   userName: String;
   userEmail: String;
   userFirstName: String;
@@ -18,18 +35,26 @@ export class ProfileComponent implements OnInit {
   state: String;
   city: String;
   street: String;
+  newPassword: String;
+  public errorMessage: string;
 
-  constructor() {
-  }
+  constructor(
+    private http: HttpClient,
+    private productService: ProductService) {}
 
   ngOnInit() {
     this.getUser();
+    this.getProducts(this.userId);
+    console.log(this.newPassword);
   }
+
+  
 
   public getUser() {
      
     let userString = localStorage.getItem("user");
     let userObject = JSON.parse(userString);
+    this.userId = userObject.id;
     this.userName = userObject.username;
     this.userEmail = userObject.email;
     this.userFirstName = userObject.firstName;
@@ -41,5 +66,45 @@ export class ProfileComponent implements OnInit {
     this.city = credentials.city;
     this.street = credentials.street;
   }
+
+  submit(newPassword1, newPassword2) {
+    if(newPassword1.value === newPassword2.value) {  
+    console.log("New PAss OK " + newPassword1.value);
+    } else {
+      console.log("New pass not the same")
+    }
+  }
+
+  getProducts(userId) {
+    this.productService.getUserProducts(userId).
+    pipe(catchError(err => this.onProductError(err))
+  ).subscribe(product => this.onProductReceived(product))
+  }
+
+  onProductReceived(product: Product) {
+    console.log(product);
+  }
+
+  onProductError(err): Observable<any> {
+    if(err.status >= 500) {
+      this.errorMessage = 'Server error, please try again later';
+    } else {
+      this.errorMessage = 'Error loading page, please try again later';
+    }
+    return of();
+  }
+
+  /*addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        catchError(this.handleError('addHero', hero))
+      );
+  } */
+
+  /*doPOST() {
+    console.log("POST");
+    let url = `api/change-password`;
+    this.http.post(url, {moo:"foo",goo:"loo"}).subscribe(res);
+  }*/
 
 }
