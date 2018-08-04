61cf567891689c894da168d470dd5757ca5dc681
Gyorfisti <gyorfisti@gmail.com>
2018-08-04 09:23:28 +0200

    restore deleted method getAllCategories

--- project-kifli/src/app/product.service.ts
+++ project-kifli/src/app/product.service.ts
@@ -45,4 +45,8 @@ export class ProductService {
     return this.http.get<ProductListDto>('/api/products/user/' + id);
   }
 
+  public getAllCategories(): Observable<any> {
+    return this.http.get('/api/categories');
+  }
+
 }
